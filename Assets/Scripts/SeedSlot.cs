8109d05fb48b13ecbf5237796441905d72e2592e
Luz Fernández Acuña <luz.fernandezac@gmail.com>
2023-02-05 15:03:16 -0500

    se continua mecánica de semillas

--- Assets/Scripts/SeedSlot.cs
+++ Assets/Scripts/SeedSlot.cs
@@ -7,10 +7,18 @@ public class SeedSlot : MonoBehaviour
 
     public bool available;
     public bool is_filled;
+    public Sprite root;
+    public Sprite sleepingSeed;
+    public GameObject dispenser;
+    public Transform pos;
+    [SerializeField] public GameObject Seed;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        dispenser = GameObject.Find("seedDispenser");
     }
 
     // Update is called once per frame
@@ -25,10 +33,20 @@ public class SeedSlot : MonoBehaviour
         {
             if (collision.gameObject.tag.Equals("Seed"))
             {
-                Destroy(collision);
-                // cambio a sprite de raices
-                // respawn semilla
+                //Destroy(collision.gameObject);
+                this.GetComponent<SpriteRenderer>().sprite = root;
+                collision.gameObject.GetComponent<SpriteRenderer>().sprite = sleepingSeed;
+                this.is_filled = true;
             }
         }
+        else if (!available)
+        {
+            if (collision.gameObject.tag.Equals("Seed"))
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+        Instantiate(Seed, pos.position, pos.rotation);
+
     }
 }
